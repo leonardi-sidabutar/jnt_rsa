c91562ed27c39e077336e3b9e93f865cfdf59ebd
Depanri Purba <depanri.purba@gmail.com>
2024-05-23 18:17:05 +0700

    update judul

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="apple-touch-icon" sizes="76x76" href="<?=base_url('assets')?>/assets/img/apple-icon.png">
     <title>
-        Sistem Pakar Endokrin
+        Kriptografi RSA
     </title>
     <!--     Fonts and icons     -->
     <link rel="stylesheet" type="text/css"
