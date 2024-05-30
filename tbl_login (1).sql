d45efdfc19e3a94a52e5a663a28a1ef127297ef3
Nama Anda <leonardisidabutar@gmail.com>
2024-05-30 08:38:50 +0700

    update tbl_login

---
+++ tbl_login (1).sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Waktu pembuatan: 30 Bulan Mei 2024 pada 01.37
+-- Versi server: 8.2.0
+-- Versi PHP: 8.1.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_jntrsa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_login`
+--
+
+DROP TABLE IF EXISTS `tbl_login`;
+CREATE TABLE IF NOT EXISTS `tbl_login` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` text COLLATE utf8mb4_general_ci NOT NULL,
+  `role` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tbl_login`
+--
+
+INSERT INTO `tbl_login` (`id`, `username`, `password`, `role`) VALUES
+(1, 'admin', '$2y$10$l6DTnLT9rK5Xjb2d8.Zu7e.uyhrl2h6YuGvZkdkOQr3o.bzMQTqd.', 'Admin'),
+(2, 'desmi', '$2y$10$Ut2MF6fBQkldafqevOEoN.4IfLqG5c5Ibac24wFdglPGAK62bQsq.', '');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
